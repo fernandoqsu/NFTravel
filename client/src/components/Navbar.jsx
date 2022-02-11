26c9c2187a3e10096c1eef29a05ba82e06a8361b
Fernando Quispe Su <fernando.qsu@gmail.com>
2022-02-10 19:04:40 -0500

    navbar v1

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,56 @@
-const Navbar = () => {
+import { useState } from 'react';
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/Ai';
+
+import logo from '../../images/logo.png';
+
+const NavbarItem = ({title, classprops}) =>{
     return(
-        <h1>Navbar</h1>
+        <li className={`mx-4 cursor-pointer divide-solid hover:border-b-2 hover:border-amber-300 hover:text-amber-300 duration-100 ${classprops}`}>
+            {title}
+        </li>
+    );
+}
+
+const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    return(
+        <nav className='w-full flex md:justify-around justify-between items-center p-4'>
+            <div className='flex-initial justify-center items-center'>
+                <h1 className='cursor-pointer text-white text-3xl font-black text-amber-300 hover:animate-pulse duration-200 transition ease-in-out'>
+                    NFTravel
+                </h1>
+            </div>
+            <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial '>
+                {["Showroom", "Tours", "Roadmap"].map ( (item, index)=> (
+                    <NavbarItem key={item + index} title ={item} />
+                ))}                
+            </ul>
+            <button className='text-black md:flex hidden list-none flex-row justify-between items-center flex-initial py-2 px-7 mx-4 rounded-full cursor-pointer bg-gradient-to-r from-amber-200 to-amber-500 hover:from-amber-500 hover:to-amber-200 hover:animate-pulse transition ease-in-out'>
+                Join us
+            </button>
+            <div className='flex relative'>
+                {toggleMenu
+                    ? <AiOutlineClose fontSize={20} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)}/>
+                    : <HiMenuAlt4 fontSize={20} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>
+                }
+                {toggleMenu && (
+                    <ul
+                        className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                        flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in animation-200
+                        "
+                    >
+                        <li className='text-xl w-full my-2'>
+                            <AiOutlineClose onClick={()=> setToggleMenu(false)}/> 
+                        </li>
+                        {["Showroom", "Tours", "Roadmap"].map ( (item, index)=> (
+                            <NavbarItem key={item + index} title ={item} classprops="my-2 text-lg"/>
+                        ))}   
+                    </ul>
+                )}
+            </div>
+        </nav>
     )
 }
 
